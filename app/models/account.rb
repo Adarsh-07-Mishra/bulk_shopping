c4d32975e0e19cc31489676e249df2ceaae93624
sharmag2101 <deepak@simplyalliancess.com>
2024-02-07 00:49:20 +0530

    [S-G] Finaly account controller

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,7 @@
 class Account < ApplicationRecord
+
+  has_one_attached :profile_pic
   enum role: [:customer, :owner]
   enum status: [:pending, :approved]
-	validates :mobile_number, uniqueness: true
+	validates :mobile_number, uniqueness: true, presence: true
 end
